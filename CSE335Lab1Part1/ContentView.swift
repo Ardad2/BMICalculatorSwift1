a0addebe6eaf90a487c3c558b8e3d8c214e33cfc
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-01-23 19:06:42 -0700

    added bmi statuses

--- CSE335Lab1Part1/ContentView.swift
+++ CSE335Lab1Part1/ContentView.swift
@@ -11,9 +11,11 @@ struct ContentView: View {
     
     @State var height: String = "";
     @State var weight: String = "";
-    private var BMI: Float { (Float(height) ?? 0) + (Float(weight) ?? 0) }
+    //private var BMI: Float { (Float(height) ?? 0) + (Float(weight) ?? 0) }
+    private var BMI: Float { (Float(weight) ?? 0) / ( (Float(height) ?? 0) * (Float(height) ?? 0) ) * 703 }
     @State var BMIString: String = "";
     @State var status: String = ""
+    @STate var bmiSTa
     
     
     var body: some View {
@@ -42,8 +44,31 @@ struct ContentView: View {
         }
 
         Spacer()
+        
+        HStack{
+            Text(status).foregroundColor(Color.blue);
+        }
+        
         Button(action: {
             self.BMIString = "\(self.BMI)"
+            
+            if (BMI < 18)
+            {
+                status = "You are Underweight..."
+            }
+            
+            else if (BMI >= 18 && BMI < 25 )
+            {
+                status = "You are Normal..."
+            }
+            else if (BMI >= 25 && BMI <= 30 )
+            {
+                status = "You are Pre-Obese..."
+            }
+            else if (BMI > 30)
+            {
+                status = "You are Obsee..."
+            }
         }) {
             Text("Calculate BMI")
         }
